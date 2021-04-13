f26690acffa75cfcfa15eb05b6d55f2264b7d75b
Phong Lo <phonglo724@gmail.com>
2021-04-13 11:25:43 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Ugly from './images/27.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>title</h1>
+        <p>quotes</p>
+        <img src={Ugly} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// image
+// quote/lyrics/poetry
+// send to friend
